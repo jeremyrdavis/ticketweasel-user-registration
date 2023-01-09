2c8b25a94edd972c0afab63f73565ca982d1dd38
jeremyrdavis <jeremyrdavis@mac.com>
2023-01-09 15:08:41 -0500

    initial

--- src/test/java/com/ticketweasel/UserRegistrationResourceTest.java
+++ src/test/java/com/ticketweasel/UserRegistrationResourceTest.java
@@ -1,14 +1,15 @@
 package com.ticketweasel;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.with;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class UserRegistrationResourceTest {
 
+    @Test
     public void testHelloEndpoint() {
         with().body(new UserRecord("Lemmy", "Kilminster", "lemmy@motorhead.com"))
                 .post("/register")
